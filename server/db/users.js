0c922483ef7fab73101341a3b410e2263cea9979
oliver-harcourt <oliver.harcourt@gmail.com>
2018-11-26 16:31:26 +1300

    15th steps finished

--- server/db/users.js
+++ server/db/users.js
@@ -1,16 +1,21 @@
 const environment = process.env.NODE_ENV || 'development'
 const config = require('../../knexfile')[environment]
 const connection = require('knex')(config)
+const hash = require('../auth/hash')
 
 module.exports = {
   createUser,
-  userExists
+  userExists,
+  getUserByName
 }
 
 function createUser (username, password, conn) {
+  console.log("createuser has been called")
+  const passwordHash = hash.generate(password)
+  console.log("passwordHash: " + passwordHash)
   const db = conn ||connection
   return db('users')
-  .insert({username, hash:password })
+  .insert({username, hash:passwordHash })
 }
 
 function userExists(username, conn) {
@@ -22,4 +27,12 @@ function userExists(username, conn) {
   return count[0].n >0
 })
 
+}
+
+function getUserByName (username, conn) {
+  const db = conn || connection
+  return db('users')
+    .select()
+    .where('username', username)
+    .first()
 }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -10,16 +10,17 @@ router.use(express.json())
 router.post('/register', register)
 
 function register (req, res) {
+  console.log(req.body)
   userExists(req.body.username)
   .then(exists => {
     if (exists) {
       return res.status(400).send({ message: 'User exists' })
     }
-    createUser(req.body.username, req.body.password)
+    createUser(req.body.username, req.body.hash)
     .then(() => res.status(201).end())
   })
   .catch(err => {
-    res.status(500).send({ message: err.message })
+    res.status(500).send({ message: err.message + "this is the catch in auth" })
   })
 }
 
